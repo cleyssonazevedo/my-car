6c01502702eac581e63d81fca3a470c6bfd201a3
Cleysson Azevedo <cleyssonazevedo@outlook.com>
2020-06-24 08:11:45 -0300

    Correção link wp

--- src/app/pages/carro/carro.component.ts
+++ src/app/pages/carro/carro.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Car } from 'src/app/model/car';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-carro',
@@ -15,7 +16,8 @@ export class CarroComponent {
     active = 0;
 
     constructor(
-        readonly route: ActivatedRoute
+        readonly route: ActivatedRoute,
+        private readonly sanitizer: DomSanitizer
     ) {
         this.route.data.subscribe(({ carro }) => this.carro = carro);
     }
@@ -25,5 +27,31 @@ export class CarroComponent {
             behavior: 'smooth',
             block: 'start'
         });
+    }
+
+    prev() {
+        if (this.active === 0) {
+            this.active = this.carro.imagens.length - 1;
+        } else {
+            this.active = this.active--;
+        }
+    }
+
+    next() {
+        if (this.carro.imagens.length - 1 === this.active) {
+            this.active = 0;
+        } else {
+            this.active = this.active++;
+        }
+    }
+
+    getMessage() {
+        return `Eu quero entrar em contato com você pelo carro ${ this.carro.marca } ${ this.carro.modelo }`;
+    }
+
+    getLink() {
+        const phone = '+5511967872631';
+        const link = `https://api.whatsapp.com/send?phone=${phone}&text=${this.getMessage()}`;
+        return this.sanitizer.bypassSecurityTrustResourceUrl(link);
     }
 }
